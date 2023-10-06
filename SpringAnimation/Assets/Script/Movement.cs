32fb0b03e1ad5f3eb86a83eef96f02a084124247
LuisNietoD <107162215+LuisNietoD@users.noreply.github.com>
2023-10-06 16:15:57 +0200

    Proto spring

--- SpringAnimation/Assets/Script/Movement.cs
+++ SpringAnimation/Assets/Script/Movement.cs
@@ -1,24 +1,37 @@
 using System;
 using System.Collections;
+using RotaryHeart.Lib.PhysicsExtension;
 using UnityEngine;
+using Physics = UnityEngine.Physics;
 
 public class Movement : MonoBehaviour
 {
+    [Header("Settings")]
     public float jumpForce = 10f;
     public float speed = 5f;
     public float rotationSpeed = 0.2f;
+    public float retractDuration = 0.3f;
     public float gravity = -9.81f;
-    
+
+    [Space]
+    [Header("Needed references")]
     public Camera playerCamera;
     public MeshMorphing spring;
-    public AnimationCurve springEffect;
-    public AnimationCurve preGround;
-    
+
+    [Space]
+    [Header("Raycast settings")]
     public float maxRayDistance = 2f; 
     public float minRayDistance = 0.5f;
 
-    public float extendDuration = 0.3f;
-    public bool retracted = true;
+    private bool retracted = true;
+    public bool bounced = false;
+    
+    private float restWeight = 0.1f;      // Weight for the rest position
+    private float compressedWeight = 0f;  // Weight for the compressed position
+    private float extendedWeight = 0.3f;  // Weight for the extended position
+
+    private Coroutine retractCoroutine;    // Coroutine to retract the spring
+
 
     private Rigidbody _rb;
 
@@ -33,10 +46,13 @@ public class Movement : MonoBehaviour
         Move();
         
         RetractTest();
-
+        
         Physics.gravity = new Vector3(0, gravity, 0);
     }
 
+    /// <summary>
+    /// Move the player
+    /// </summary>
     void Move()
     {
         // Movement input
@@ -63,68 +79,77 @@ public class Movement : MonoBehaviour
 
     void RetractTest()
     {
-        //Shoot a ray down
+        float thickness = 0.5f;
         Vector3 rayOrigin = transform.position;
         Vector3 rayDirection = Vector3.down;
-
         RaycastHit hit;
-
-        if (Physics.Raycast(rayOrigin, rayDirection, out hit, maxRayDistance))
+        
+        
+        if (Physics.SphereCast(rayOrigin,thickness, rayDirection, out hit, maxRayDistance))
         {
-            //Check if you hit the ground
             GameObject hitObject = hit.collider.gameObject;
+
             Debug.DrawLine(rayOrigin, hit.point, Color.red);
-            if (hitObject.CompareTag("Ground"))
+            if (hitObject.CompareTag("Ground") && bounced)
             {
-                // Calculate the weight using linear interpolation
-                float weight = Mathf.Lerp(0f, 0.3f, Mathf.InverseLerp(minRayDistance, maxRayDistance, hit.distance));
-
-                // Clamp the weight to the desired range
-                weight = Mathf.Clamp(weight, 0f, 0.3f);
-
+                //Extend the spring before jumping
+                retracted = false;
+                float weight = Mathf.Lerp(compressedWeight, extendedWeight, Mathf.InverseLerp(minRayDistance, maxRayDistance, hit.distance));
+                weight = Mathf.Clamp(weight, compressedWeight, extendedWeight);
                 spring.m_MorphTargets[0].Weight = weight;
             }
-            else
+            else if (hitObject.CompareTag("Ground"))
             {
-                spring.m_MorphTargets[0].Weight = 0.3f;
+                //Only retract the spring before landing
+                float weight = Mathf.Lerp(extendedWeight, compressedWeight, Mathf.InverseLerp(minRayDistance, maxRayDistance, hit.distance));
+                weight = Mathf.Clamp(weight, compressedWeight, spring.m_MorphTargets[0].Weight);
+                spring.m_MorphTargets[0].Weight = weight;
             }
+        }
+        else if (!retracted)
+        {
+            retracted = true;
+            bounced = false;
+            // Stop the coroutine if exist
+            if (retractCoroutine != null)
+            {
+                StopCoroutine(retractCoroutine);
+            }
+
+            retractCoroutine = StartCoroutine(RetractSpring());
         }
     }
     
     private void OnCollisionEnter(Collision collision)
     {
-        //Start extend when collide the ground
-        if (collision.gameObject.CompareTag("Ground"))
+        //Start extend and jump when collide the ground
+        if (collision.gameObject.CompareTag("Ground") && !bounced)
         {
+            _rb.velocity = new Vector3(-_rb.velocity.x, 0, _rb.velocity.z);
             _rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-            //StopCoroutine(RetractSpring());
-            //StartCoroutine(ExtendSpring());
+            bounced = true;
         }
     }
-
-    //Extend animation curve based
-    IEnumerator ExtendSpring()
-    {
-        float elapsedTime = 0.0f;
-        while (elapsedTime < extendDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            spring.m_MorphTargets[0].Weight = springEffect.Evaluate(elapsedTime/extendDuration);
-            yield return null;
-        }
-
-        retracted = false;
-    }
+    
     
     //retract animation curve based
     IEnumerator RetractSpring()
     {
-        float elapsedTime = 0.0f;
-        while (spring.m_MorphTargets[0].Weight > 0)
+        float elapsedTime = 0f;
+
+        //retract over time
+        while (elapsedTime < retractDuration)
         {
+            float weight = Mathf.Lerp(extendedWeight, restWeight, elapsedTime / retractDuration);
+            spring.m_MorphTargets[0].Weight = weight;
+
             elapsedTime += Time.deltaTime;
-            spring.m_MorphTargets[0].Weight = preGround.Evaluate(elapsedTime/extendDuration);
             yield return null;
         }
+
+        // Set the weight to rest weight
+        spring.m_MorphTargets[0].Weight = restWeight;
+        
+        retractCoroutine = null;
     }
 }
